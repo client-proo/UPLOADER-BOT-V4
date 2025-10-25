c4b515375f5c9e9c1228919b22536101ddbafc07
client-proo <client.proo2024@gmail.com>
2025-10-25 12:06:31 +0330

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -10,11 +10,11 @@ logging.basicConfig(
 )
 
 class Config(object):
-    
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8279094169:AAESX85YG88sZKSPqgZuiQbOBVmJoQmhq_s")
-    API_ID = int(os.environ.get("API_ID", "29534256"))
-    API_HASH = os.environ.get("API_HASH", "8a2b0ee3e07f6903bff02dd53cb93ff8")
-    
+
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", ))
+    API_HASH = os.environ.get("API_HASH", "")
+
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     MAX_FILE_SIZE = 2194304000
     TG_MAX_FILE_SIZE = 2194304000
@@ -22,7 +22,7 @@ class Config(object):
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
-    
+
     OUO_IO_API_KEY = ""
     MAX_MESSAGE_LENGTH = 4096
     PROCESS_MAX_TIMEOUT = 3600
@@ -30,16 +30,16 @@ class Config(object):
 
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 
-    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://asgarim316:3lgglfXo8mfOti5p@filetolink.p585kyz.mongodb.net/?retryWrites=true&w=majority&appName=Filetolink")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "")
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002613485817"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", "1623757589"))
-    SESSION_NAME = "LinkBoltBot"
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002613485817")
+    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    SESSION_NAME = "UploaderXNTBot"
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 
     TG_MIN_FILE_SIZE = 2194304000
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Linktofile45bot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     ADL_BOT_RQ = {}
 
     # Set False off else True
@@ -52,4 +52,3 @@ class Config(object):
     # Verification video link
     VERIFICATION = os.environ.get("VERIFICATION", "")
 
-    
